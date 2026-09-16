0eeeabe9d20ec567e82e52f67abb9d3038d741df
jwechols <echolsjm@gmail.com>
2026-09-16 10:06:09 -0600

    Mercy hub stays her real school page.

--- app/kids/[kid]/hub/page.tsx
+++ app/kids/[kid]/hub/page.tsx
@@ -1,4 +1,4 @@
-import { notFound, redirect } from "next/navigation";
+import { notFound } from "next/navigation";
 import SchoolhouseHub from "@/components/SchoolhouseHub";
 import { KIDS, type KidId } from "@/lib/kids";
 
@@ -9,6 +9,5 @@ interface Props {
 export default async function KidHubPage({ params }: Props) {
   const { kid } = await params;
   if (!KIDS[kid as KidId]) notFound();
-  if (kid === "mercy") redirect("/kids/mercy/den");
   return <SchoolhouseHub kidId={kid} />;
 }
